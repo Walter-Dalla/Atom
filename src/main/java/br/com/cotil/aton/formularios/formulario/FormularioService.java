262dd49261b0cdc594e07c5e562375ab8b52e394
Walter <wwddttnn@hotmail.com>
2019-11-06 23:19:31 -0200

    FIX: Formulario pode pesquisar por comparação

--- src/main/java/br/com/cotil/aton/formularios/formulario/FormularioService.java
+++ src/main/java/br/com/cotil/aton/formularios/formulario/FormularioService.java
@@ -37,7 +37,9 @@ public class FormularioService {
 
 	public Page<FormularioModel> listaFormularios(UsuarioModel usuario, Integer id, String nomeFormulario,
 			boolean ativo, Integer page, Integer size) throws BadRequestException {
-
+		
+		nomeFormulario = '%'+nomeFormulario+'%';
+		
 		Page<FormularioModel> lista = formularioRepository.findByIdAndNomeFormularioAndIdUsuarioAndAtivo(id,
 				nomeFormulario, usuario.getId(), ativo, Utils.setPageRequestConfig(page, size));
 		return lista;
